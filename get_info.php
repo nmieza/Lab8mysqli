e5e9dcee8c517a0eda9a7d17ff105da194173145
nmieza <nurmieza14@gmail.com>
2022-06-05 15:35:06 +0800

    commit

---
+++ get_info.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="add user.php" method = "post">
+        Firstname: <input type="text" name="firstname" required><br>
+        Lastname: <input type="text" name="lastname" required><br>
+        E-mail: <input type="text" name="email" reguired><br>
+        Password: <input type="text" name="password" required><br>
+        Registration date: <input type="date" name="reg date" required><br>
+        <input type="submit" value="Submit Data">
+    </form>
+</body>
+</html>
